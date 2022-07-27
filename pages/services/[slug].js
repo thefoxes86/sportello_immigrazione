687ee2aadbf821ab328612c705df357c20819b70
thefoxes86 <nicola.volpi86@gmail.com>
2022-07-27 14:53:28 +0200

    Fix problem on build

--- pages/services/[slug].js
+++ pages/services/[slug].js
@@ -2,83 +2,95 @@ import Layout from '../../components/Layout'
 import { lang } from '../../content/services'
 import { getAllServices, getSingleService } from '../../utils/getAllServices'
 import Link from 'next/link'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import styles from '../../styles/Single.module.css'
 
 const Post = ({ data }) => {
-  const [detail, setDetail] = useState(JSON.parse(data))
+  const [detail, setDetail] = useState()
+
+  useEffect(() => {
+    setDetail(JSON.parse(data))
+  }, [data])
 
   return (
     <Layout>
-      <h1 className={styles.titleSingle}>{detail.text}</h1>
+      {detail ? (
+        <>
+          <h1 className={styles.titleSingle}>{detail.text}</h1>
 
-      {detail.data.documenti && (
-        <div className={styles.containerDocumenti}>
-          {detail.data.documenti.necessari && (
-            <>
-              <h2 className={styles.titleDocumenti}>DOCUMENTI NECESSARI:</h2>
-              {detail.data.documenti.necessari.map((item, index) => (
-                <p key={index} className={styles.itemList}>
-                  <span className={styles.pointList}></span> {item.title}
-                </p>
-              ))}
-            </>
+          {detail.data.documenti && (
+            <div className={styles.containerDocumenti}>
+              {detail.data.documenti.necessari && (
+                <>
+                  <h2 className={styles.titleDocumenti}>
+                    DOCUMENTI NECESSARI:
+                  </h2>
+                  {detail.data.documenti.necessari.map((item, index) => (
+                    <p key={index} className={styles.itemList}>
+                      <span className={styles.pointList}></span> {item.title}
+                    </p>
+                  ))}
+                </>
+              )}
+              {detail.data.documenti.domestico && (
+                <>
+                  <h2 className={styles.titleDocumenti}>
+                    IN CASO DI LAVORO DOMESTICO :
+                  </h2>
+                  {detail.data.documenti.domestico.map((item, index) => (
+                    <p key={index} className={styles.itemList}>
+                      <span className={styles.pointList}></span> {item.title}
+                    </p>
+                  ))}
+                </>
+              )}
+              {detail.data.documenti.dipendente && (
+                <>
+                  <h2 className={styles.titleDocumenti}>
+                    IN CASO DI SOCIO DIPENDENTE :
+                  </h2>
+                  {detail.data.documenti.dipendente.map((item, index) => (
+                    <p key={index} className={styles.itemList}>
+                      <span className={styles.pointList}></span> {item.title}
+                    </p>
+                  ))}
+                </>
+              )}
+            </div>
           )}
-          {detail.data.documenti.domestico && (
-            <>
-              <h2 className={styles.titleDocumenti}>
-                IN CASO DI LAVORO DOMESTICO :
-              </h2>
-              {detail.data.documenti.domestico.map((item, index) => (
-                <p key={index} className={styles.itemList}>
-                  <span className={styles.pointList}></span> {item.title}
-                </p>
-              ))}
-            </>
+
+          {/* Show advice Empty */}
+          {detail.data.adviceEmpty && (
+            <div className={styles.containerAdviceEmpty}>
+              {detail.data.adviceEmpty.text}
+            </div>
           )}
-          {detail.data.documenti.dipendente && (
-            <>
-              <h2 className={styles.titleDocumenti}>
-                IN CASO DI SOCIO DIPENDENTE :
-              </h2>
-              {detail.data.documenti.dipendente.map((item, index) => (
-                <p key={index} className={styles.itemList}>
-                  <span className={styles.pointList}></span> {item.title}
-                </p>
-              ))}
-            </>
+
+          {/* Show Advice Full with background */}
+          {detail.data.adviceFull && (
+            <div className={styles.containerAdviceFull}>
+              {detail.data.adviceFull.text}
+            </div>
           )}
-        </div>
-      )}
 
-      {/* Show advice Empty */}
-      {detail.data.adviceEmpty && (
-        <div className={styles.containerAdviceEmpty}>
-          {detail.data.adviceEmpty.text}
-        </div>
-      )}
+          {/* Square Button for calling  */}
+          {detail.data.numberToCall && (
+            <div className={styles.callSquare}>
+              <p>Per questo servizio chiamare:</p>
+              <p>+39 0584 634891</p>
+            </div>
+          )}
 
-      {/* Show Advice Full with background */}
-      {detail.data.adviceFull && (
-        <div className={styles.containerAdviceFull}>
-          {detail.data.adviceFull.text}
-        </div>
+          {/* Came back Button */}
+          <Link href="/">
+            <a className={styles.cameBackButton}>
+              <div>ALL SERVICES</div>
+            </a>
+          </Link>
+        </>
+      ) : (
+        'Loading'
       )}
-
-      {/* Square Button for calling  */}
-      {detail.data.numberToCall && (
-        <div className={styles.callSquare}>
-          <p>Per questo servizio chiamare:</p>
-          <p>+39 0584 634891</p>
-        </div>
-      )}
-
-      {/* Came back Button */}
-      <Link href="/">
-        <a className={styles.cameBackButton}>
-          <div>ALL SERVICES</div>
-        </a>
-      </Link>
     </Layout>
   )
 }
